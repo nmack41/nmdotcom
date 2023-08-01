86c487112489034ac2fb9ef55d97eb4018796f82
Nick Mackowski <nmack41@gmail.com>
2023-07-31 20:47:57 -0400

    Add API

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
   <NuxtLayout class="flex flex-col h-screen pt-24">
     <NuxtPage class="flex-grow overflow-auto" />
+    <div>{{ message }}</div>
   </NuxtLayout>
 </template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      message: "",
+    };
+  },
+  async mounted() {
+    const { text } = await (await fetch("/api/message")).json();
+    this.message = text;
+  },
+};
+</script>
