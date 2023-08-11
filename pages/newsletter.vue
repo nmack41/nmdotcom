9c63d59c3b16254b2470487490beac9269f6f832
Nick Mackowski <nmack41@gmail.com>
2023-08-11 19:46:28 -0400

    updated newsletter.vue

--- pages/newsletter.vue
+++ pages/newsletter.vue
@@ -1,10 +1,3 @@
-<script>
-const storage = require("azure-storage");
-const connectionString =
-  "DefaultEndpointsProtocol=https;AccountName=cosmosdb-newsletter;AccountKey=2zVJWI8UB4yF8Bvjlf8U0zugz6nUdebWc6zR3BIR9reIlRTIYnIhvtVIJW67KnynWUviQYdNOzOpACDbnBacRg==;TableEndpoint=https://cosmosdb-newsletter.table.cosmos.azure.com:443/;";
-const storageClient = storage.createTableService(connectionString);
-</script>
-
 <template>
   <div class="p-4">
     I will send a monthly (maybe bi-monthly) newsletter regarding health,
